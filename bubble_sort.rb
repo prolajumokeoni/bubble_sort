c320ab441ffc63a283ebc0c3431b32a5a35c0371
priscilla <prolajumokeoni@gmail.com>
2021-04-27 12:01:22 +0100

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,45 +1,44 @@
 def bubble_sort(arr)
-	num_of_time_to_sort = arr.length - 1
-	1.upto(arr.length - 1) do
-		i = 0
-		while i < num_of_time_to_sort 					
-			if arr[i] > arr[i + 1]
-				arr_i_cpy = arr[i]
-				arr[i] = arr[i + 1]
-				arr[i + 1] = arr_i_cpy
-			end
-			i += 1			
-		end
-		num_of_time_to_sort -= 1
-	end
-	arr
+  num_of_time_to_sort = arr.length - 1
+  1.upto(arr.length - 1) do
+    i = 0
+    while i < num_of_time_to_sort
+      if arr[i] > arr[i + 1]
+        arr_i_cpy = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = arr_i_cpy
+      end
+      i += 1
+    end
+    num_of_time_to_sort -= 1
+  end
+  arr
 end
 
 {}
 
-def bubble_sort_by(arr)	
-	num_of_time_to_sort = arr.length - 1
-	1.upto(arr.length - 1) do
-		i = 0
-		while i < num_of_time_to_sort
-			if block_given?
-				sort_order = yield(arr[i], arr[i + 1])				
-			else
-				sort_order = arr[i] - arr[i + 1]
-			end	
-			if sort_order.positive?
-				arr_i_cpy = arr[i]
-				arr[i] = arr[i + 1]
-				arr[i + 1] = arr_i_cpy
-			end
-			i += 1			
-		end
-		num_of_time_to_sort -= 1
-	end
-	arr
+def bubble_sort_by(arr)
+  num_of_time_to_sort = arr.length - 1
+  1.upto(arr.length - 1) do
+    i = 0
+    while i < num_of_time_to_sort
+      sort_order = if block_given?
+                     yield(arr[i], arr[i + 1])
+                   else
+                     arr[i] - arr[i + 1]
+                   end
+      if sort_order.positive?
+        arr_i_cpy = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = arr_i_cpy
+      end
+      i += 1
+    end
+    num_of_time_to_sort -= 1
+  end
+  arr
 end
 
 p bubble_sort [7, 2, 0, 0, 10, 1, 2, 3]
 
-p bubble_sort_by([7, 2, 0, 0, 10, 1, 2,]) { |a, b| a<=>b }
-
+p bubble_sort_by([7, 2, 0, 0, 10, 1, 2]) { |a, b| a <=> b }
